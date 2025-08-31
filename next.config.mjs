dd957802b9fada092cd058a9b110dcdbb5861099
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-09-01 03:29:12 +0900

    feat: puppeteer for Vercel
    https://vercel.com/guides/deploying-puppeteer-with-nextjs-on-vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const config = {
   devIndicators: false,
+  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
 };
 
 export default config;
--- src/libs/tappy.ts
+++ src/libs/tappy.ts
@@ -2,8 +2,26 @@ import { type Device, Tappy } from "@lycorp-jp/tappy";
 import { PuppeteerAdapter } from "@lycorp-jp/tappy/adapters";
 import puppeteer from "puppeteer";
 
+const setupPuppeteer = async () => {
+  const isVercel = !!process.env.VERCEL_ENV;
+
+  if (isVercel) {
+    const chromium = (await import("@sparticuz/chromium")).default;
+    return await puppeteer.launch({
+      headless: true,
+      args: chromium.args,
+      executablePath: await chromium.executablePath(),
+    });
+  } else {
+    return await puppeteer.launch({
+      headless: true,
+      channel: "chrome",
+    });
+  }
+};
+
 export const analyze = async (url: string, device: Device) => {
-  const browser = await puppeteer.launch({ headless: true, channel: "chrome" });
+  const browser = await setupPuppeteer();
   const page = await browser.newPage();
 
   const adapter = new PuppeteerAdapter(page);
