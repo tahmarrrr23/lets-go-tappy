0caafb5fbdc6c5c33cdb7e82cb0073db3c3cb2fd
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-09-01 03:41:48 +0900

    chore: add tappy to serverExternalPackages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const config = {
   devIndicators: false,
-  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+  serverExternalPackages: [
+    "@sparticuz/chromium",
+    "puppeteer-core",
+    "@lycorp-jp/tappy",
+  ],
 };
 
 export default config;
